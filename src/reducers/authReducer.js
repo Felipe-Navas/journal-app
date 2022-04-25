c75b618a855ff8266ec9333e21af7bbca3b40d8f
Felipe Navas <felipenavas.itec@gmail.com>
2022-04-25 20:59:03 -0300

    Adding redux

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+import { types } from '../styles/types';
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case types.login:
+      return { uid: action.payload.uid, name: action.payload.displayName };
+
+    case types.logout:
+      return {};
+
+    default:
+      return state;
+  }
+};
