3b0b86d322b53246455e9ffb899e135ff39b3b71
Felipe Navas <felipenavas.itec@gmail.com>
2022-04-30 14:41:47 -0300

    Adding edit note in firebase and refactoring useForm hook

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import { NotesAppBar } from './NotesAppBar';
+import React, { useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote } from '../../actions/notes'
+import { useForm } from '../../hooks/useForm'
+import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+  const dispatch = useDispatch()
+  const { active: note } = useSelector((state) => state.notes)
+  const [formValues, handleInputChange, reset] = useForm(note)
+
+  const { body, title } = formValues
+
+  const activeId = useRef(note.id)
+
+  useEffect(() => {
+    if (note.id !== activeId.current) {
+      reset(note)
+      activeId.current = note.id
+    }
+  }, [note, reset])
+
+  useEffect(() => {
+    dispatch(activeNote(formValues.id, { ...formValues }))
+  }, [formValues, dispatch])
+
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -12,20 +34,28 @@ export const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          name="title"
+          value={title}
+          onChange={handleInputChange}
         />
 
         <textarea
           placeholder="What happened today?"
           className="notes__textarea"
+          name="body"
+          value={body}
+          onChange={handleInputChange}
         ></textarea>
 
-        <div className="notes__image">
-          <img
-            src="https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?"
-            alt="Imagen"
-          />
-        </div>
+        {note.url && (
+          <div className="notes__image">
+            <img
+              src="https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?"
+              alt="Imagen"
+            />
+          </div>
+        )}
       </div>
     </div>
-  );
-};
+  )
+}
