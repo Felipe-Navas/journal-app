558fbccf19b8480df39c2bcfcdbebaeb2e5f8571
Felipe Navas <felipenavas.itec@gmail.com>
2022-05-08 18:56:32 -0300

    Fixing loadNotes test

--- src/tests/helpers/loadNotes.test.js
+++ src/tests/helpers/loadNotes.test.js
@@ -1,17 +1,22 @@
+/** * @jest-environment node */
 import { db } from '../../firebase/firebase-config'
 import { loadNotes } from '../../helpers/loadNotes'
 
-jest.mock('../../firebase/firebase-config', () => {
-  const response = ['1', 'active']
-  return {
-    db: { collection: jest.fn(() => response) },
-  }
-})
 
 describe('Testing the loadNotes helper', () => {
   test('should return an array of notes', async () => {
-    const response = await loadNotes('1')
-    // TODO: fix this test
-    console.log(response)
+    const response = await loadNotes('testing')
+    expect(Array.isArray(response)).toBe(true)
+    expect(response[0]).toEqual({
+      id: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+      title: expect.any(String),
+    })
   })
+
+  test('should return an empty array when pass an invalid uid', async () => {
+    const response = await loadNotes('testing1')
+    expect(response).toEqual([])
+   })
 })
