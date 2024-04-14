ce080d6bd927041d31dfd0171220a5d2887ab579
Felipe Navas <felipenavas.itec@gmail.com>
2024-04-14 11:52:27 -0300

    Fix login Google Icon

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -80,7 +80,7 @@ export const LoginScreen = () => {
             <div className="google-icon-wrapper">
               <img
                 className="google-icon"
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
                 alt="google button"
               />
             </div>
